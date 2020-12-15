adc18485cf69ba88c14ce3bca4889d28d5b1f151
airi32490 <milcrea313@gmail.com>
2020-12-15 15:47:31 +0900

    確認機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2020_12_14_094242) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "name", null: false
-    t.string "checker_id", null: false
+    t.integer "checker_id", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
