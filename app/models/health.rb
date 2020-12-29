56eaa56995b32553d6d2b6103a4be1cc341d2c3c
airi32490 <milcrea313@gmail.com>
2020-12-29 15:10:22 +0900

    marketモデルのバリデーション設定

--- app/models/health.rb
+++ app/models/health.rb
@@ -5,9 +5,11 @@ class Health < ApplicationRecord
   belongs_to :condition
 
   with_options presence: true do
-    validates :body_temperature, numericality: { only_float: true, message: 'は半角数字で入力してください' }
     validates :condition_id, numericality: { other_than: 1, message: 'を選択してください' }
-    validates :alcohol_level, numericality: { only_float: true, message: 'は半角数字で入力してください' }
     validates :user_id
+    with_options numericality: { only_float: true, message: 'は半角数字で入力してください' } do
+      validates :body_temperature
+      validates :alcohol_level
+    end
   end
 end
