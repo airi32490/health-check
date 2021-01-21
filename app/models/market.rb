0d9fc07b0fcedac6e197a4e6c0e01b28d6f3b477
airi32490 <milcrea313@gmail.com>
2021-01-21 15:17:33 +0900

    マーケットモデルのバリデーション修正

--- app/models/market.rb
+++ app/models/market.rb
@@ -2,7 +2,7 @@ class Market < ApplicationRecord
   belongs_to :user
 
   with_options presence: true do
-    with_options format: { with: /\A(\d{4,5})(\.[0-9]{2}?)\z/, message: "は半角数字で入力してください" } do
+    with_options format: { with: /\A(\d{4,5})(\.[0-9]{0,2}?)\z/, message: "は半角数字で入力してください" } do
       validates :nikkei
       validates :ny_dow
     end
